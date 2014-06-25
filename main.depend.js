4cc448dcd12256beacd370b87162cde5d96b27eb
Simon Philouze <Contact@simon-philouze.fr>
2014-06-25 09:23:47 -0700

    Version 0.5 Gesture
    - Adding comments

--- main.depend.js
+++ main.depend.js
@@ -1,20 +1,20 @@
-function stateDevice(controller)
+function stateDevice(controller) // Check if the leapmotion device is connected or not
 {
-	var connected = function(controller)
+	var connected = function(controller) // Methode if connected
 	{
 		controller.on('deviceConnected', function(){console.log("Leap Motion Connected");});
 	};
 
-	var disconnected = function(controller)
+	var disconnected = function(controller) // Methode if is disconnected
 	{
 		controller.on('deviceDisconnected', function(){console.log("Leap Motion Disconnected");});
 	};
 
-	connected(controller);
+	connected(controller); 
 	disconnected(controller);
 }
 
-function time()
+function time() // this function return the local timestamp.
 {
 	var date = new Date();
 	return date.getTime();
--- main.js
+++ main.js
@@ -1,28 +1,54 @@
+/*
+/!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ 
+TO UNDERSTAND THIS CODE YOU MUST HAVE UNDERSTOOD THE RUNNING OF THE LEAP MOTION
+/!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ 
+*/
+/*
+    -           -150   0   150           +
+		  ........|....|....|.........
+        ..........|....|....|..........
+      ............|....|....|............
+    ..............|....|....|.............
+   ...............|....|....|...............
+  ................|....|....|................
+ .................|....|....|.................
+..................|....|....|..................
+............... __|____|____|__................
+...............|_Leap__motion__|...............
+*/
+
+// Create controller
 var controller = new Leap.Controller();
 
-var date = time();
-var sizeUp = 120;
-var timeUp = 400;
+// Define time
+var date = time(); 
 
+// Controle Varaiable
+var sizeUp = 120; // The margin location 0
+var timeUp = 400; // Time between capture
+
+// Start detect leap motion and write a log
 stateDevice(controller);
 
+// Main Loop
 Leap.loop(function(frame)
 {
+	// If one hand is detected
 	if(frame.hands.length = 1)
 	{
 		var hand = frame.hands[0];
 
 		if(hand)
 		{	
-			if((time() - date) > timeUp)
+			if((time() - date) > timeUp)// if ( now - start date ) is higher than timeUp
 			{
-				if(hand.sphereCenter[0] < -sizeUp)
+				if(hand.sphereCenter[0] < -sizeUp) // If center of hand is lower than -sizeUp
 				{
-					console.log("LEFT: " + hand.sphereCenter[0]);
+					console.log("LEFT: " + hand.sphereCenter[0]); // write in log LEFT
 				}
-				else if(hand.sphereCenter[0] > sizeUp)
+				else if(hand.sphereCenter[0] > sizeUp) // If center of hand is higher than sizeUp
 				{
-					console.log("RIGHT : " + hand.sphereCenter[0]);
+					console.log("RIGHT : " + hand.sphereCenter[0]); // write in log RIGHT
 				}
 
 				date = time();
